2d415affc6f892c55bee9de517ca781c3db5683a
AkhilKushe <kusheakhil@gmail.com>
2022-06-01 02:11:26 +0530

    Added peek functionality to nets and inport

--- core/sitar_net.h
+++ core/sitar_net.h
@@ -100,6 +100,17 @@ class net:public base_net
 				_num_tokens--;
 				return 1;
 			}
+		
+		bool peek(token<_width>& tok)
+			//peek a token and return 1 if net is not empty
+			//return 0 otherwise
+			{
+				if (empty()) return 0; //can't pull
+				//else
+				assert(_buffer);
+				tok=_buffer[_front];
+				return 1;
+			}
 
 		//constructor
 		net()
